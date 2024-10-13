40c21dff8c17ed796e4204fb30ad6d3bca73b908
François Szczepkowski <francois.szczepkowski@expansion.be>
2024-10-13 14:42:41 +0200

    update : page.jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Index() {
+	return (
+		<div className={styles.page}>
+			<main className={styles.main}>
+				<h1>Accueil</h1>
+			</main>
+		</div>
+	);
+}
